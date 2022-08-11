55f6548c056e0e42c36ed6fd807f4ae3cbb38e04
KuraoHikari <dewaindra705@gmail.com>
2022-08-11 15:07:57 +0700

    next side bar

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { FiSettings } from 'react-icons/fi';
 import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 import './App.css';
+import { Navbar, Footer, Sidebar, ThemeSettings } from './components';
+import { Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Customers, Kanban, Area, Bar, Pie, Financial, ColorPicker, ColorMapping, Editor } from './pages';
 
 const App = () => {
   const activeMenu = true;
@@ -17,7 +19,15 @@ const App = () => {
               </button>
             </TooltipComponent>
           </div>
-          {activeMenu ? <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white">Sidebar</div> : <div className="w-0 dark:bg-secondary-dark-bg">Sidebar</div>}
+          {activeMenu ? (
+            <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white">
+              <Sidebar />
+            </div>
+          ) : (
+            <div className="w-0 dark:bg-secondary-dark-bg">
+              <Sidebar />
+            </div>
+          )}
           <div className={`dark:bg-main-bg bg-main-bg min-h-screen w-full ${activeMenu ? 'md:ml-72 ' : 'flex-2'}`}>
             <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full">Navbar</div>
           </div>
